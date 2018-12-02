43da5de22dc62aa5f9a7988aec5ed5c3ecbb1539
Jessica Chao <jchao@brg.com>
2018-12-01 23:46:34 -0800

    landing-services WIP

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,7 @@ import './../styles/Landing.css';
 import Slider from "react-slick";
 import '../../node_modules/slick-carousel/slick/slick.css';
 import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import {MdBuild, MdAccessAlarm, MdList, MdModeEdit, MdExtension} from 'react-icons/md'
 
 class Landing extends Component {
   render () {
@@ -42,38 +43,100 @@ class Landing extends Component {
         <section className="landing-intro">
           <div className="inner">
             <div className="content">
-              <h1>VCO APD</h1>
-                <p>
-                  Architecture walls have become so popular for VCO that we staff a dedicated division to assist with anything from a single glass storefront to a fast-paced, multi-floor undertaking. VCO California is a licensed “B” Contractor.
-                </p>
-              <a className="btn" href="/"><span>Learn More</span></a>
+              <h2>
+                Demountable partitions are a perfect solution designed for function, aesthetic and flexibility.
+              </h2>
+              <a className="btn" href="/">SEE PRODUCTS</a>
             </div>
           </div>
         </section>
 
         <section className="landing-story">
             <div className="point">
-              <h2 className="point-title">Our Story</h2>
+              <a className="btn-team" href="/about">Meet the team</a>
+              <h2 className="point-title">ARCHITECTURAL PRODUCTS DIVISION</h2>
+              <h3 className="point-title2">A division of Vangard Concept Offices</h3>
               <p className="point-description">
-                Vangard Concept Offices, now simplified as VCO, started as a team of eight and grew to over 800 by the late 1990’s. VCO, previously Business Interiors (BI) then Business Resource Group (BRG), worked with some of the most innovative companies from Cisco and Polycom to Netflix and Silicon Valley Bank.
-                Over three decades, we have grown into the largest Teknion dealer in North America. Our custom workplace projects can be found in hundreds of offices around the world.
-                VCO continues to manage long term partnerships with the industry’s highest level of service.
+                  Create a quiet private office, meeting room or telephone room. Quickly reconfigure and upgrade walls to adapt to changes in the workforce, and integrate your own new or existing technologies with ease. Architecture walls have become so popular for VCO that we staff a dedicated division to assist with anything from a single glass storefront to a fast-paced, multi-floor undertaking.
               </p>
+              <p>VCO APD is a California licensed “B” Contractor.</p>
             </div>
         </section>
+
+        <hr className="horizontal-line"></hr>
 
         <section className="landing-services">
             <div className="point">
               <h2 className="point-title">Services</h2>
-                <ul>
-                  <li>Design</li>
-                  <li>Product solutions</li>
-                  <li>Project Management</li>
-                  <li>Installation and Reconfiguration</li>
-                  <li>Inventory</li>
-                </ul>
+                <table className="table">
+                  <tr>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td scope="col"><MdModeEdit className="icon"></MdModeEdit></td>
+                          </tr>
+                          <tr>
+                            <td data-header="design"><p>Design</p></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td scope="col"><MdExtension className="icon"></MdExtension></td>
+                          </tr>
+                          <tr>
+                            <td data-header="design"><p>Product Solutions</p></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td scope="col"><MdAccessAlarm className="icon"></MdAccessAlarm></td>
+                          </tr>
+                          <tr>
+                            <td data-header="design"><p>Project Management</p></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td scope="col"><MdBuild className="icon"></MdBuild></td>
+                          </tr>
+                          <tr>
+                            <td data-header="design"><p>Installation & Reconfiguration</p></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                    <td>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td scope="col"><MdList className="icon"></MdList></td>
+                          </tr>
+                          <tr>
+                            <td data-header="design"><p>Inventory</p></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              <a className="btn-services" href="/services">See more services</a>
             </div>
         </section>
+
+        <hr className="horizontal-line"></hr>
 
         <section className="landing-projects">
             <div className="point">
